f694b4870669692633900223a1a06fe5ca495a3a
Seb <m.sebi97@yahoo.com>
2023-01-12 12:50:26 +0100

    mobile-sidebar Done.

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,19 +1,21 @@
-
+import { motion } from "framer-motion" 
 const Landing = () => {
   return (
     <div className="flex flex-col font-epilogue text-center items-center">
         <img src="../assets/images/image-hero-mobile.png" alt="landing-logo"/>
-        <h3 className="font-bold text-[40px] mt-8">Make remote work</h3>
-        <p className="text-[16px] text-medium-gray  px-4 mt-3">Get your team in sync, no matter your location. Streamline processes, 
-            create team rituals, and watch productivity soar.
-        </p>
-        <button className="bg-almost-black text-almost-white px-4 py-2 mt-4 rounded-xl">Learn More</button>
-        <div className="flex justify-center  mt-10 gap-4 w-full mx-8">
-            <img className="h-[20px] w-[80px]" src="../assets/images/client-databiz.svg" alt="databiz-logo"/>
-            <img className="h-[20px] w-[80px]" src="../assets/images/client-audiophile.svg" alt="audiophile-logo"/>
-            <img className="h-[20px] w-[80px]" src="../assets/images/client-meet.svg" alt="meet-logo"/>
-            <img className="h-[20px] w-[80px]" src="../assets/images/client-maker.svg" alt="maker-logo"/>
-        </div>
+        
+          <h3 className="font-bold text-[40px] mt-8">Make remote work</h3>
+        
+          <p className="text-[16px] text-medium-gray  px-4 mt-3">Get your team in sync, no matter your location. Streamline processes, 
+              create team rituals, and watch productivity soar.
+          </p>
+          <button className="bg-almost-black text-almost-white px-4 py-2 mt-8 rounded-xl">Learn More</button>
+          <div className="flex justify-center  mt-16 gap-4 w-full mx-8">
+              <img className="h-[20px] w-[80px]" src="../assets/images/client-databiz.svg" alt="databiz-logo"/>
+              <img className="h-[20px] w-[80px]" src="../assets/images/client-audiophile.svg" alt="audiophile-logo"/>
+              <img className="h-[20px] w-[80px]" src="../assets/images/client-meet.svg" alt="meet-logo"/>
+              <img className="h-[20px] w-[80px]" src="../assets/images/client-maker.svg" alt="maker-logo"/>
+          </div>
     </div>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,10 +5,18 @@ function Navbar(
   
 ) {
   const [isMenuToggled, setIsMenuToggled] = useState(false);
+  const [isFeatures, setIsFeatures] = useState(false);
+  const [isCompany, setIsCompany] = useState(false);
+
   const isDesktop = useMediaQuery("(min-width: 768px)");
-
-
   const hover = "hover:text-almost-black cursor-pointer transition duration-300"
+
+  // Close feature and company dropwdowns when closing the sidebar
+  function handleClick() {
+    setIsMenuToggled(!isMenuToggled)
+    setIsFeatures(false)
+    setIsCompany(false)
+  }
   return (
 
 
@@ -48,7 +56,8 @@ function Navbar(
             <div className=" fixed right-0 top-0 bottom-0 h-full bg-white w-[220px]">
               {/* CLOSE ICON */}
               <div className="flex justify-end pt-10 pr-10">
-                <button onClick={() => setIsMenuToggled(!isMenuToggled)}>
+                {/* <button onClick={() => setIsMenuToggled(!isMenuToggled)}> */}
+                <button onClick={handleClick}>
                 <img  src="../assets/images/icon-close-menu.svg" alt="icon-close-menu"/>
                 </button>
               </div>
@@ -64,10 +73,12 @@ function Navbar(
                 }}
               >
 
-
-              <div className="flex flex-col items-start text-[16px] text-medium-gray gap-4   ml-[10%] mt-[12%] text-2xl h-5/6">                
-                <p className={`${hover} `}>Features <span className="mb-1 ml-3 content-arrowDown"></span></p>
-                <div className="flex  flex-col  gap-2 h-1/6 text-sm pl-[20] ml-16 w-full">
+              <div className="flex flex-col items-start text-[16px] text-medium-gray gap-4   ml-[10%] mt-[12%] text-2xl h-5/6">
+                {}               
+                <button onClick={() => setIsFeatures(!isFeatures)} className={`${hover} `}>
+                  Features <span className={`${isFeatures ? 'content-arrowUp' : 'content-arrowDown'} mb-1 ml-3`}></span>                  
+                </button>
+                <div className={`${isFeatures ? "" : 'hidden'} flex   flex-col  gap-2 h-1/6 text-sm pl-[20] ml-12 w-full`}>
                   <div className="flex ">
                     <img className="mr-4" src="../assets/images/icon-todo.svg" alt="todo-list-icon"/>
                     <button>Todo List</button>
@@ -85,8 +96,9 @@ function Navbar(
                     <button>Planning</button>
                   </div>
                 </div>
-                <p className={`${hover}`}>Company <span className="mb-1 ml-3 content-arrowDown"></span></p>
-                <div className="flex flex-col gap-4   h-1/6 pl-[20] ml-16 w-full text-sm">
+                <button  onClick={() => setIsCompany(!isCompany)} className={`${hover}`}>Company <span className={`${isCompany ? 'content-arrowUp' : 'content-arrowDown'} mb-1 ml-3`}></span>
+                </button>                
+                <div className={`${isCompany ? "" : 'hidden'} flex  flex-col  gap-4 h-1/6 pl-[20]   ml-12 w-full text-sm`}>
                     <p>History</p>
                     <p>Our Team</p>
                     <p>Blog</p>
@@ -94,9 +106,12 @@ function Navbar(
                 <p className={`${hover}`}>Careers</p>
                 <p className={`${hover}`}>About</p> 
 
-              <div className="flex flex-col   w-full px-8 text-[18px]">
+              <div className="flex flex-col  w-full px-8 mt-[20%] text-[18px]">
                   <button className={`${hover}`}>Login</button>
                   <button className={`${hover} border-2    border-medium-gray hover:border-almost-black  rounded-2xl`}>Register</button>
+                <p className="text-xs text-center mt-[45%]" >(Buttons are not actually clickable, the only purpose of this website 
+                  was to learn Tailwindcss.)
+                </p>
               </div>
             </div>
           </motion.div>
