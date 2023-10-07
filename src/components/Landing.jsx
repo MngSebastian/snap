4f741816db9b1874e5114449a11d350a4ca4de31
Sebastian Mosneagu <44436863+MngSebastian@users.noreply.github.com>
2023-10-07 09:56:21 +0200

    Stylistic changes to company logos

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,55 +1,102 @@
-import { motion } from "framer-motion";
 import useMediaQuery from "../hooks/useMediaQuery";
 const Landing = () => {
   const isDesktop = useMediaQuery("(min-width: 768px)");
 
   return (
     <div>
-
-    {isDesktop ? (
-      <div className="flex h-full w-screen  flex-row-reverse justify-center pt-8">
-        <div className="flex  pr-24">
-          <img className="w-[650px] h-[700px] " src="../assets/images/image-hero-desktop.png" alt="landing-logo"/>
-        </div>
-        <div className="flex items-center   w-4/6 flex-col">
-          <div>
-            <h3 className="font-bold text-[70px] mt-8">Make</h3>
-            <h3 className="font-bold text-[70px] mt-[-38px]"> remote work</h3>
-
-            <p className="text-[19px] text-medium-gray   mt-8">Get your team in sync, no matter your location.<br />
-            Streamline processes, 
-                create team rituals, and <br/>  watch productivity soar.
-            </p>
-
-          <button className="border-2 w-[130px] border-almost-black text-almost-black hover:bg-almost-black hover:text-almost-white
-             transition duration-300 font-bold  py-2 mt-16 mr-64 rounded-xl">Learn More</button>
-          <div className="flex justify-center  mt-48 gap-4 w-full ">
-              <img className="h-[30px] w-[100px]" src="../assets/images/client-databiz.svg" alt="databiz-logo"/>
-              <img className="h-[30px] w-[100px]" src="../assets/images/client-audiophile.svg" alt="audiophile-logo"/>
-              <img className="h-[30px] w-[100px]" src="../assets/images/client-meet.svg" alt="meet-logo"/>
-              <img className="h-[30px] w-[100px]" src="../assets/images/client-maker.svg" alt="maker-logo"/>
+      {isDesktop ? (
+        <div className="flex h-full w-screen flex-row-reverse justify-center pt-8">
+          <div className="flex pr-24">
+            <img
+              className="w-[650px] md:h-[570px] xl:h-[620px]"
+              src="../assets/images/image-hero-desktop.png"
+              alt="landing-logo"
+            />
           </div>
+          <div className="flex items-center h-3/6 w-4/6 flex-col">
+            <div className="">
+              <h3 className="font-bold text-[70px] mt-8">Make</h3>
+              <h3 className="font-bold text-[70px] mt-[-38px]"> remote work</h3>
+
+              <p className="text-[19px] text-medium-gray   mt-8">
+                Get your team in sync, no matter your location.
+                <br />
+                Streamline processes, create team rituals, and <br /> watch
+                productivity soar.
+              </p>
+
+              <button
+                className="border-2 w-[130px] border-almost-black text-almost-black hover:bg-almost-black hover:text-almost-white
+             transition duration-300 font-bold  py-2 mt-16 mr-64 rounded-xl"
+              >
+                Learn More
+              </button>
+              <div className="flex justify-center w-full md:mt-32  lg:mt-32 xl:mt-48 gap-4">
+                <img
+                  className="h-[30px] w-[100px]"
+                  src="../assets/images/client-databiz.svg"
+                  alt="databiz-logo"
+                />
+                <img
+                  className="h-[30px] w-[100px]"
+                  src="../assets/images/client-audiophile.svg"
+                  alt="audiophile-logo"
+                />
+                <img
+                  className="h-[30px] w-[100px]"
+                  src="../assets/images/client-meet.svg"
+                  alt="meet-logo"
+                />
+                <img
+                  className="h-[30px] w-[100px]"
+                  src="../assets/images/client-maker.svg"
+                  alt="maker-logo"
+                />
+              </div>
+            </div>
           </div>
         </div>
-    </div>
       ) : (
-      <div className="flex flex-col font-epilogue text-center items-center">
-        <img src="../assets/images/image-hero-mobile.png" alt="landing-logo"/>
-        <h3 className="font-bold text-[40px] mt-8">Make remote work</h3>
-        <p className="text-[16px] text-medium-gray  px-4 mt-3">Get your team in sync, no matter your location. Streamline processes, 
-            create team rituals, and watch productivity soar.
-        </p>
-        <button className="bg-almost-black text-almost-white px-4 py-2 mt-8 rounded-xl">Learn More</button>
-        <div className="flex justify-center  mt-16 gap-4 w-full mx-8">
-            <img className="h-[20px] w-[80px]" src="../assets/images/client-databiz.svg" alt="databiz-logo"/>
-            <img className="h-[20px] w-[80px]" src="../assets/images/client-audiophile.svg" alt="audiophile-logo"/>
-            <img className="h-[20px] w-[80px]" src="../assets/images/client-meet.svg" alt="meet-logo"/>
-            <img className="h-[20px] w-[80px]" src="../assets/images/client-maker.svg" alt="maker-logo"/>
-        </div>
-      </div>
-    )}
-    </div>
-  )
-}
+        <div className="flex flex-col font-epilogue text-center items-center">
+          <img
+            src="../assets/images/image-hero-mobile.png"
+            alt="landing-logo"
+          />
+          <h3 className="font-bold text-[40px] mt-8">Make remote work</h3>
+          <p className="text-[16px] text-medium-gray  px-4 mt-3">
+            Get your team in sync, no matter your location. Streamline
+            processes, create team rituals, and watch productivity soar.
+          </p>
+          <button className="bg-almost-black text-almost-white px-4 py-2 mt-8 rounded-xl">
+            Learn More
+          </button>
 
-export default Landing
+          <div className="flex justify-center gap-6 m-8">
+            <img
+              className="h-[20px] w-[80px]"
+              src="../assets/images/client-databiz.svg"
+              alt="databiz-logo"
+            />
+            <img
+              className="h-[20px] w-[80px]"
+              src="../assets/images/client-audiophile.svg"
+              alt="audiophile-logo"
+            />
+            <img
+              className="h-[20px] w-[80px]"
+              src="../assets/images/client-meet.svg"
+              alt="meet-logo"
+            />
+            <img
+              className="h-[20px] w-[80px]"
+              src="../assets/images/client-maker.svg"
+              alt="maker-logo"
+            />
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Landing;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,171 +1,242 @@
-import { useState } from "react"
+import { useState } from "react";
 import useMediaQuery from "../hooks/useMediaQuery";
-import {motion} from "framer-motion"
-function Navbar( 
-  
-) {
+import { motion } from "framer-motion";
+function Navbar() {
   const [isMenuToggled, setIsMenuToggled] = useState(false);
   const [isFeatures, setIsFeatures] = useState(false);
   const [isCompany, setIsCompany] = useState(false);
 
   const isDesktop = useMediaQuery("(min-width: 768px)");
-  const hover = "hover:text-almost-black cursor-pointer transition duration-300"
+  const hover =
+    "hover:text-almost-black cursor-pointer transition duration-300";
 
   // Close feature and company dropwdowns when closing the sidebar
   function handleClick() {
-    setIsMenuToggled(!isMenuToggled)
-    setIsFeatures(false)
-    setIsCompany(false)
+    setIsMenuToggled(!isMenuToggled);
+    setIsFeatures(false);
+    setIsCompany(false);
   }
   return (
-
-
-
-
-    <div className="flex font-epilogue  justify-between w-screen py-4">
+    <div className="flex font-epilogue border-b-2 justify-between w-screen py-4">
       {/* DESKTOP NAV */}
-        {isDesktop ? (
-          <div className="flex justify-between w-full">
-            <div className="flex  w-[75%] text-medium-gray items-center h-10 gap-4 md:gap-12  pl-4">
-              <img src="../assets/images/logo.svg" alt="snap-logo"/>
-              {/* features DropDown Btn */}
-              <button onClick={() => setIsFeatures(!isFeatures)} className={`${hover} `}>
-                Features <span className={`${isFeatures ? 'content-arrowUp' : 'content-arrowDown'} `}></span>                  
+      {isDesktop ? (
+        <div className="flex justify-between w-full">
+          <div className="flex  w-[75%] text-medium-gray items-center h-10 gap-4 md:gap-12  pl-4">
+            <img src="../assets/images/logo.svg" alt="snap-logo" />
+            {/* features DropDown Btn */}
+            <button
+              onClick={() => setIsFeatures(!isFeatures)}
+              className={`${hover} `}
+            >
+              Features{" "}
+              <span
+                className={`${
+                  isFeatures ? "content-arrowUp" : "content-arrowDown"
+                } `}
+              ></span>
+            </button>
+            <div
+              className={`${
+                isFeatures ? "" : "hidden"
+              } flex justify-center shadow-[0px_0px_20px_1px_rgba(0,0,0,0.1)] absolute bg-white 
+                flex-col mt-48  ml-20 gap-2 h-[128px] text-sm pl-4 rounded-xl w-36`}
+            >
+              <div className="flex ">
+                <img
+                  className="mr-4"
+                  src="../assets/images/icon-todo.svg"
+                  alt="todo-list-icon"
+                />
+                <button>Todo List</button>
+              </div>
+              <div className="flex ">
+                <img
+                  className="mr-4"
+                  src="../assets/images/icon-calendar.svg"
+                  alt="calendar-icon"
+                />
+                <button>Calendar</button>
+              </div>
+              <div className="flex">
+                <img
+                  className="mr-4"
+                  src="../assets/images/icon-reminders.svg"
+                  alt="reminders-icon"
+                />
+                <button>Reminders</button>
+              </div>
+              <div className="flex">
+                <img
+                  className="mr-4"
+                  src="../assets/images/icon-planning.svg"
+                  alt="planning-icon"
+                />
+                <button>Planning</button>
+              </div>
+            </div>
+
+            {/* Company button Dropdown */}
+            <button
+              onClick={() => setIsCompany(!isCompany)}
+              className={`${hover}`}
+            >
+              Company{" "}
+              <span
+                className={`${
+                  isCompany ? "content-arrowUp" : "content-arrowDown"
+                } `}
+              ></span>
+            </button>
+            <div
+              className={`${
+                isCompany ? "" : "hidden"
+              } flex justify-center shadow-[0px_0px_20px_1px_rgba(0,0,0,0.1)] absolute bg-white 
+                flex-col mt-44  ml-64 gap-2 h-28 text-sm pl-4 rounded-xl w-24`}
+            >
+              <p>History</p>
+              <p>Our Team</p>
+              <p>Blog</p>
+            </div>
+
+            <p className={`${hover}`}>Company</p>
+            <p className={`${hover}`}>Careers</p>
+            <p className={`${hover}`}>About</p>
+          </div>
+          <div className="flex justify-end w-[220px] pr-6 text-medium-gray">
+            <button className={`${hover} pr-8`}>Login</button>
+            <button
+              className={`${hover} border-2    border-medium-gray hover:border-almost-black  rounded-2xl px-4`}
+            >
+              Register
+            </button>
+          </div>
+        </div>
+      ) : (
+        <div className="flex  justify-between w-full mx-4">
+          <img src="../assets/images/logo.svg" alt="snap-logo" />
+          <button
+            className="flex justify-end cursor:pointer px-4"
+            onClick={() => setIsMenuToggled(!isMenuToggled)}
+          >
+            <img src="../assets/images/icon-menu.svg" alt="menu-icon" />
+          </button>
+        </div>
+      )}
+      {/* MOBILE MENU POPUP */}
+      {!isDesktop && isMenuToggled && (
+        <div className=" fixed right-0 top-0 bottom-0 h-full bg-white w-[220px]">
+          {/* CLOSE ICON */}
+          <div className="flex justify-end pt-10 pr-10">
+            <button onClick={handleClick}>
+              <img
+                src="../assets/images/icon-close-menu.svg"
+                alt="icon-close-menu"
+              />
+            </button>
+          </div>
+          {/* MENU ITEMS */}
+          <motion.div
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.5 }}
+            transition={{ duration: 0.3 }}
+            variants={{
+              hidden: { opacity: 0, x: 40 },
+              visible: { opacity: 1, x: 0 },
+            }}
+          >
+            <div className="flex flex-col items-start text-[16px] text-medium-gray gap-4   ml-[10%] mt-[12%] text-2xl h-5/6">
+              {}
+              <button
+                onClick={() => setIsFeatures(!isFeatures)}
+                className={`${hover} `}
+              >
+                Features{" "}
+                <span
+                  className={`${
+                    isFeatures ? "content-arrowUp" : "content-arrowDown"
+                  } mb-1 ml-3`}
+                ></span>
               </button>
-              <div className={`${isFeatures ? "" : 'hidden'} flex justify-center shadow-[0px_0px_20px_1px_rgba(0,0,0,0.1)] absolute bg-white 
-                flex-col mt-48  ml-20 gap-2 h-[128px] text-sm pl-4 rounded-xl w-36`}>
+              <div
+                className={`${
+                  isFeatures ? "" : "hidden"
+                } flex   flex-col  gap-2 h-1/6 text-sm pl-[20] ml-12 w-full`}
+              >
                 <div className="flex ">
-                  <img className="mr-4" src="../assets/images/icon-todo.svg" alt="todo-list-icon"/>
+                  <img
+                    className="mr-4"
+                    src="../assets/images/icon-todo.svg"
+                    alt="todo-list-icon"
+                  />
                   <button>Todo List</button>
                 </div>
                 <div className="flex ">
-                  <img className="mr-4" src="../assets/images/icon-calendar.svg" alt="calendar-icon"/>
+                  <img
+                    className="mr-4"
+                    src="../assets/images/icon-calendar.svg"
+                    alt="calendar-icon"
+                  />
                   <button>Calendar</button>
                 </div>
                 <div className="flex">
-                  <img className="mr-4" src="../assets/images/icon-reminders.svg" alt="reminders-icon"/>
+                  <img
+                    className="mr-4"
+                    src="../assets/images/icon-reminders.svg"
+                    alt="reminders-icon"
+                  />
                   <button>Reminders</button>
                 </div>
                 <div className="flex">
-                  <img className="mr-4" src="../assets/images/icon-planning.svg" alt="planning-icon"/>
+                  <img
+                    className="mr-4"
+                    src="../assets/images/icon-planning.svg"
+                    alt="planning-icon"
+                  />
                   <button>Planning</button>
                 </div>
               </div>
-              
-              
-              {/* Company button Dropdown */}
-              <button  onClick={() => setIsCompany(!isCompany)} className={`${hover}`}>
-                Company <span className={`${isCompany ? 'content-arrowUp' : 'content-arrowDown'} `}></span>
-              </button>
-              <div className={`${isCompany ? "" : 'hidden'} flex justify-center shadow-[0px_0px_20px_1px_rgba(0,0,0,0.1)] absolute bg-white 
-                flex-col mt-44  ml-64 gap-2 h-28 text-sm pl-4 rounded-xl w-24`}>
-                    <p>History</p>
-                    <p>Our Team</p>
-                    <p>Blog</p>
-              </div>
-              
-              
-              <p className={`${hover}`}>Company</p>
-              <p className={`${hover}`}>Careers</p>
-              <p className={`${hover}`}>About</p> 
-
-            </div>
-            <div className="flex justify-end w-[220px] pr-6 text-medium-gray">
-                <button className={`${hover} pr-8`}>Login</button>
-                <button className={`${hover} border-2    border-medium-gray hover:border-almost-black  rounded-2xl px-4`}>Register</button>
-            </div>
-
-
-          </div>
-          
-        ) : (
-          <div className="flex  justify-between w-full mx-4">
-            <img src="../assets/images/logo.svg" alt="snap-logo"/>
-            <button 
-              className="flex justify-end cursor:pointer px-4"
-              onClick={() => setIsMenuToggled(!isMenuToggled)}>
-              <img src="../assets/images/icon-menu.svg" alt="menu-icon"/>
-            </button>
-          </div>
-        )}
-          {/* MOBILE MENU POPUP */}
-          {!isDesktop && isMenuToggled && (
-            <div className=" fixed right-0 top-0 bottom-0 h-full bg-white w-[220px]">
-              {/* CLOSE ICON */}
-              <div className="flex justify-end pt-10 pr-10">
-                <button onClick={handleClick}>
-                <img  src="../assets/images/icon-close-menu.svg" alt="icon-close-menu"/>
-                </button>
-              </div>
-            {/* MENU ITEMS */}
-              <motion.div
-                initial="hidden"
-                whileInView="visible"
-                viewport={{ once: true, amount: 0.5 }}
-                transition={{ duration: 0.3 }}
-                variants={{
-                  hidden: { opacity: 0, x: 40 },
-                  visible: { opacity: 1, x: 0 },
-                }}
+              <button
+                onClick={() => setIsCompany(!isCompany)}
+                className={`${hover}`}
               >
-
-              <div className="flex flex-col items-start text-[16px] text-medium-gray gap-4   ml-[10%] mt-[12%] text-2xl h-5/6">
-                {}               
-                <button onClick={() => setIsFeatures(!isFeatures)} className={`${hover} `}>
-                  Features <span className={`${isFeatures ? 'content-arrowUp' : 'content-arrowDown'} mb-1 ml-3`}></span>                  
-                </button>
-                <div className={`${isFeatures ? "" : 'hidden'} flex   flex-col  gap-2 h-1/6 text-sm pl-[20] ml-12 w-full`}>
-                  <div className="flex ">
-                    <img className="mr-4" src="../assets/images/icon-todo.svg" alt="todo-list-icon"/>
-                    <button>Todo List</button>
-                  </div>
-                  <div className="flex ">
-                    <img className="mr-4" src="../assets/images/icon-calendar.svg" alt="calendar-icon"/>
-                    <button>Calendar</button>
-                  </div>
-                  <div className="flex">
-                    <img className="mr-4" src="../assets/images/icon-reminders.svg" alt="reminders-icon"/>
-                    <button>Reminders</button>
-                  </div>
-                  <div className="flex">
-                    <img className="mr-4" src="../assets/images/icon-planning.svg" alt="planning-icon"/>
-                    <button>Planning</button>
-                  </div>
-                </div>
-                <button  onClick={() => setIsCompany(!isCompany)} className={`${hover}`}>Company <span className={`${isCompany ? 'content-arrowUp' : 'content-arrowDown'} mb-1 ml-3`}></span>
-                </button>                
-                <div className={`${isCompany ? "" : 'hidden'} flex  flex-col  gap-4 h-1/6 pl-[20]   ml-12 w-full text-sm`}>
-                    <p>History</p>
-                    <p>Our Team</p>
-                    <p>Blog</p>
-                </div>
-                <p className={`${hover}`}>Careers</p>
-                <p className={`${hover}`}>About</p> 
+                Company{" "}
+                <span
+                  className={`${
+                    isCompany ? "content-arrowUp" : "content-arrowDown"
+                  } mb-1 ml-3`}
+                ></span>
+              </button>
+              <div
+                className={`${
+                  isCompany ? "" : "hidden"
+                } flex  flex-col  gap-4 h-1/6 pl-[20]   ml-12 w-full text-sm`}
+              >
+                <p>History</p>
+                <p>Our Team</p>
+                <p>Blog</p>
+              </div>
+              <p className={`${hover}`}>Careers</p>
+              <p className={`${hover}`}>About</p>
 
               <div className="flex flex-col  w-full px-8 mt-[20%] text-[18px]">
-                  <button className={`${hover}`}>Login</button>
-                  <button className={`${hover} border-2    border-medium-gray hover:border-almost-black  rounded-2xl`}>Register</button>
-                <p className="text-xs text-center mt-[45%]" >(Buttons are not actually clickable, the only purpose of this website 
-                  was to learn Tailwindcss.)
+                <button className={`${hover}`}>Login</button>
+                <button
+                  className={`${hover} border-2    border-medium-gray hover:border-almost-black  rounded-2xl`}
+                >
+                  Register
+                </button>
+                <p className="text-xs text-center mt-[45%]">
+                  (Buttons are not actually clickable, the only purpose of this
+                  website was to learn Tailwindcss.)
                 </p>
               </div>
             </div>
           </motion.div>
-
-            </div>
-          )}
-
-
-
-
-
-
-
-
+        </div>
+      )}
     </div>
-  )
+  );
 }
 
-export default Navbar
-
-
+export default Navbar;
