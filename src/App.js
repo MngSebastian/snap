8a3ddff3a894cf84e7fc50074166a80e9adfb2d0
Sebastian Mosneagu <44436863+MngSebastian@users.noreply.github.com>
2023-10-07 10:16:22 +0200

    oveflow-hidden to div in app

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Landing from "./components/Landing";
 
 function App() {
   return (
-    <div className="bg-almost-white w-screen h-screen o">
+    <div className="bg-almost-white w-screen h-screen overflow-hidden">
       <Navbar />
       <Landing />
     </div>
